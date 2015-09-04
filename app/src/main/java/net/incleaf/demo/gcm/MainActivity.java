ac212344ac38e9776c8fcd71fa75a762600b701a
Hyeon-Su Lee <incleaf@gmail.com>
2015-09-04 17:43:57 +0900

    delete some code

--- app/src/main/java/net/incleaf/demo/gcm/MainActivity.java
+++ app/src/main/java/net/incleaf/demo/gcm/MainActivity.java
@@ -1,11 +1,8 @@
 package net.incleaf.demo.gcm;
 
 import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
@@ -14,11 +11,6 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
-
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -30,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar mRegistrationProgressBar;
     private BroadcastReceiver mRegistrationBroadcastReceiver;
     private TextView mInformationTextView;
-    private RestAdapter restAdapter;
-    private APIService apiService;
 
 
     @Override
@@ -39,126 +29,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        registBroadcastReceiver();
-        Log.i(TAG, "test.");
-
-        // 토큰을 보여줄 TextView를 정의
-//        mInformationTextView = (TextView) findViewById(R.id.informationTextView);
-//        mInformationTextView.setVisibility(View.GONE);
-        // 토큰을 가져오는 동안 인디케이터를 보여줄 ProgressBar를 정의
-//        mRegistrationProgressBar = (ProgressBar) findViewById(R.id.registrationProgressBar);
-//        mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
-        // 토큰을 가져오는 Button을 정의
-//        mRegistrationButton = (Button) findViewById(R.id.registrationButton);
-//        mRegistrationButton.setOnClickListener(new View.OnClickListener() {
-//            /**
-//             * 버튼을 클릭하면 토큰을 가져오는 getInstanceIdToken() 메소드를 실행한다.
-//             * @param view
-//             */
-//            @Override
-//            public void onClick(View view) {
-//                getInstanceIdToken();
-//            }
-//        });
-
-        //apiAdapter 정의
-        restAdapter = new RestAdapter.Builder()
-                .setEndpoint(URLS.API_URL)
-                .build();
-
-        apiService = restAdapter.create(APIService.class);
-
         getInstanceIdToken();
     }
 
 
-    /**
-     * 앱이 실행되어 화면에 나타날때 LocalBoardcastManager에 액션을 정의하여 등록한다.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_READY));
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_GENERATING));
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
-
-    }
-
-    /**
-     * 앱이 화면에서 사라지면 등록된 LocalBoardcast를 모두 삭제한다.
-     */
-    @Override
-    protected void onPause() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
-        super.onPause();
-    }
-
-    /**
-     * Instance ID를 이용하여 디바이스 토큰을 가져오는 RegistrationIntentService를 실행한다.
-     */
     public void getInstanceIdToken() {
         if (checkPlayServices()) {
-            // Start IntentService to register this application with GCM.
             Intent intent = new Intent(this, RegistrationIntentService.class);
             this.startService(intent);
-            Log.i("TAG","START INTETNT");
         }
     }
-
-    /**
-     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
-     */
-    public void registBroadcastReceiver(){
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                Log.i(TAG, action);
-
-                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
-//                    // 액션이 READY일 경우
-//                    mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
-//                    mInformationTextView.setVisibility(View.GONE);
-                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
-                    // 액션이 GENERATING일 경우
-//                    mRegistrationProgressBar.setVisibility(ProgressBar.VISIBLE);
-//                    mInformationTextView.setVisibility(View.VISIBLE);
-//                    mInformationTextView.setText(getString(R.string.registering_message_generating));
-                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
-                    // 액션이 COMPLETE일 경우
-//                    mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
-//                    mRegistrationButton.setText(getString(R.string.registering_message_complete));
-//                    mRegistrationButton.setEnabled(false);
-                    String token = intent.getStringExtra("token");
-//                    mInformationTextView.setText(token);
-
-                    post(token);
-                }
-
-            }
-        };
-    }
-
-    private void post(String deviceToken){
-
-        apiService.postDeviceToken(deviceToken, new Callback<Response>() {
-            @Override
-            public void success(Response response, Response response2) {
-                Log.e("success", ""+response.getBody().toString());
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.e("fail", ""+error);
-            }
-        });
-    }
-
-
 
     /**
      * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
--- app/src/main/java/net/incleaf/demo/gcm/QuickstartPreferences.java
+++ app/src/main/java/net/incleaf/demo/gcm/QuickstartPreferences.java
@@ -5,8 +5,8 @@ package net.incleaf.demo.gcm;
  */
 public class QuickstartPreferences {
 
-    public static final String REGISTRATION_READY = "registrationReady";
-    public static final String REGISTRATION_GENERATING = "registrationGenerating";
-    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+//    public static final String REGISTRATION_READY = "registrationReady";
+//    public static final String REGISTRATION_GENERATING = "registrationGenerating";
+//    public static final String REGISTRATION_COMPLETE = "registrationComplete";
 
 }
